3b36a5f6f69a128d6fe002cfe25d01cfd0f3a0b0
“Xiaojiang <lixiaojiang.gis@gmail.com>
2019-03-01 19:54:11 -0500

    updated code

--- Treepedia/createPoints.py
+++ Treepedia/createPoints.py
@@ -3,7 +3,6 @@
 # Copyright(C) Ian Seiferling, Xiaojiang Li, Marwa Abdulhai, Senseable City Lab, MIT 
 # First version July 21 2017
 
-
 # now run the python file: createPoints.py, the input shapefile has to be in projection of WGS84, 4326
 def createPoints(inshp, outshp, mini_dist):
     
@@ -18,6 +17,7 @@ def createPoints(inshp, outshp, mini_dist):
         output: the result point feature class
         mini_dist: the minimum distance between two created point
 
+    modified by May 2ed, 2018, consider the linestring and multi-linear string
     last modified by Xiaojiang Li, MIT Senseable City Lab
     
     '''
@@ -32,8 +32,9 @@ def createPoints(inshp, outshp, mini_dist):
     
     
     count = 0
-    s = {'trunk_link','tertiary','motorway','motorway_link','steps', None, ' ','pedestrian','primary', 'primary_link','footway','tertiary_link', 'trunk','secondary','secondary_link','tertiary_link','bridleway','service'}
-    
+    # s = {'trunk_link','tertiary','motorway','motorway_link','steps', None, ' ','pedestrian','primary', 'primary_link','footway','tertiary_link', 'trunk','secondary','secondary_link','tertiary_link','bridleway','service'}
+    s = {'trunk_link','tertiary','motorway','motorway_link','steps', ' ','pedestrian','primary', 'primary_link','footway','tertiary_link', 'trunk','secondary','secondary_link','tertiary_link','bridleway','service'}
+
     # the temporaray file of the cleaned data
     root = os.path.dirname(inshp)
     basename = 'clean_' + os.path.basename(inshp)
@@ -42,61 +43,87 @@ def createPoints(inshp, outshp, mini_dist):
     # if the tempfile exist then delete it
     if os.path.exists(temp_cleanedStreetmap):
         fiona.remove(temp_cleanedStreetmap, 'ESRI Shapefile')
+        print ('removed the existed tempfile')
     
     # clean the original street maps by removing highways, if it the street map not from Open street data, users'd better to clean the data themselve
     with fiona.open(inshp) as source, fiona.open(temp_cleanedStreetmap, 'w', driver=source.driver, crs=source.crs,schema=source.schema) as dest:
-        
         for feat in source:
             try:
                 i = feat['properties']['highway'] # for the OSM street data
+                # i = feat['properties']['fclass'] # for the OSM tokyo street data
                 if i in s:
                     continue
             except:
                 # if the street map is not osm, do nothing. You'd better to clean the street map, if you don't want to map the GVI for highways
-                key = dest.schema['properties'].keys()[0] # get the field of the input shapefile and duplicate the input feature
+                key = list(dest.schema['properties'].keys())[0] # get the field of the input shapefile and duplicate the input feature
                 i = feat['properties'][key]
                 if i in s:
                     continue
-            
+
+            # print feat
             dest.write(feat)
 
     schema = {
         'geometry': 'Point',
         'properties': {'id': 'int'},
     }
-
-    # Create pointS along the streets
-    with fiona.drivers():
+    
+    
+    # Create point along the streets
+    with fiona.Env():
         #with fiona.open(outshp, 'w', 'ESRI Shapefile', crs=source.crs, schema) as output:
         with fiona.open(outshp, 'w', crs = from_epsg(4326), driver = 'ESRI Shapefile', schema = schema) as output:
             for line in fiona.open(temp_cleanedStreetmap):
-                first = shape(line['geometry'])
-                
-                length = first.length
-                
-                try:
-                    # convert degree to meter, in order to split by distance in meter
-                    project = partial(pyproj.transform,pyproj.Proj(init='EPSG:4326'),pyproj.Proj(init='EPSG:3857')) #3857 is psudo WGS84 the unit is meter
+                try: 
+                    # deal with MultiLineString and LineString
+                    featureType = line['geometry']['type']
                     
-                    line2 = transform(project, first)
-                    linestr = list(line2.coords)
-                    dist = mini_dist #set
-                    for distance in range(0,int(line2.length), dist):
-                        point = line2.interpolate(distance)
+                    # for the LineString
+                    if featureType == "LineString":
+                        first = shape(line['geometry'])
+                        length = first.length
                         
-                        # convert the local projection back the the WGS84 and write to the output shp
-                        project2 = partial(pyproj.transform,pyproj.Proj(init='EPSG:3857'),pyproj.Proj(init='EPSG:4326'))
-                        point = transform(project2, point)
-                        output.write({'geometry':mapping(point),'properties': {'id':1}})
+                        project = partial(pyproj.transform,pyproj.Proj(init='EPSG:4326'),pyproj.Proj(init='EPSG:3857')) #3857 is psudo WGS84 the unit is meter
+                        line2 = transform(project, first)
+                        linestr = list(line2.coords)
+                        dist = mini_dist 
+                        
+                        for distance in range(0,int(line2.length), dist):
+                            point = line2.interpolate(distance)
+                            project2 = partial(pyproj.transform,pyproj.Proj(init='EPSG:3857'),pyproj.Proj(init='EPSG:4326'))
+                            point = transform(project2, point)
+                            output.write({'geometry':mapping(point),'properties': {'id':1}})
+                    
+                    # for the MultiLineString, seperate these lines, then partition those lines
+                    elif featureType == "MultiLineString":
+                        multiline_geom = shape(line['geometry'])
+                        print ('This is a multiline')
+                        for singleLine in multiline_geom:
+                            length = singleLine.length
+                            
+                            # partion each single line in the multiline
+                            project = partial(pyproj.transform,pyproj.Proj(init='EPSG:4326'),pyproj.Proj(init='EPSG:3857')) #3857 is psudo WGS84 the unit is meter
+                            line2 = transform(project, singleLine)
+                            linestr = list(line2.coords)
+                            dist = mini_dist #set
+                            
+                            for distance in range(0,int(line2.length), dist):
+                                point = line2.interpolate(distance)
+                                project2 = partial(pyproj.transform,pyproj.Proj(init='EPSG:3857'),pyproj.Proj(init='EPSG:4326'))
+                                point = transform(project2, point)
+                                output.write({'geometry':mapping(point),'properties': {'id':1}})
+                    
+                    else:
+                        continue
+                
                 except:
                     print ("You should make sure the input shapefile is WGS84")
                     return
-                    
+
     print("Process Complete")
     
     # delete the temprary cleaned shapefile
     fiona.remove(temp_cleanedStreetmap, 'ESRI Shapefile')
-
 
 # Example to use the code, 
 # Note: make sure the input linear featureclass (shapefile) is in WGS 84 or ESPG: 4326
@@ -105,10 +132,9 @@ if __name__ == "__main__":
     import os,os.path
     import sys
     
-    root = 'MYPATHH//spatial-data'
-    inshp = os.path.join(root,'CambridgeStreet_wgs84.shp')
-    outshp = os.path.join(root,'Cambridge20m.shp')
+    # set as your own filename
+    inshp = '../sample-spatialdata/CambridgeStreet_wgs84.shp'
+    outshp = '../sample-spatialdata/Cambridge20m.shp'
+    
     mini_dist = 20 #the minimum distance between two generated points in meter
     createPoints(inshp, outshp, mini_dist)
-
-
